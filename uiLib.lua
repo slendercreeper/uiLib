72bd8078325ee704e554dece4c83582ea4cd59a7
slendercreeper <95612302+slendercreeper@users.noreply.github.com>
2021-12-13 22:18:10 -0800

    Update uiLib.lua

--- uiLib.lua
+++ uiLib.lua
@@ -14,12 +14,12 @@ local utility = {}
 -- themes
 local objects = {}
 local themes = {
-	Background = Color3.fromRGB(24, 24, 24), 
-	Glow = Color3.fromRGB(255, 254, 255), 
-	Accent = Color3.fromRGB(10, 10, 10), 
-	LightContrast = Color3.fromRGB(20, 20, 20), 
-	DarkContrast = Color3.fromRGB(14, 14, 14),  
-	TextColor = Color3.fromRGB(255, 255, 255)
+    Background = stringtocolor(SettingsT.Background),
+    Glow = stringtocolor(SettingsT.Glow),
+    Accent = stringtocolor(SettingsT.Accent),
+    LightContrast = stringtocolor(SettingsT.LightContrast),
+    DarkContrast = stringtocolor(SettingsT.DarkContrast),  
+    TextColor = stringtocolor(SettingsT.TextColor)
 }
 
 do
